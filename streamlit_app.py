de12eac9b04701cf19733947672f0512d782c1c8
Nathalie Castells <nathalie.castells@rothamsted.ac.uk>
2026-07-17 11:58:37 +0100

    added sectors

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,51 +13,7 @@ st.set_page_config(
 # Declare some useful functions.
 
 @st.cache_data
-def get_gdp_data():
-    """Grab LTE Download data from a CSV file.
 
-    This uses caching to avoid having to read the file every time. If we were
-    reading from an HTTP endpoint instead of a file, it's a good idea to set
-    a maximum age to the cache with the TTL argument: @st.cache_data(ttl='1d')
-    """
-
-    # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
-    DATA_FILENAME = Path(__file__).parent/'data/gdp_data.csv'
-    raw_gdp_df = pd.read_csv(DATA_FILENAME)
-
-    MIN_YEAR = 1960
-    MAX_YEAR = 2022
-
-    # The data above has columns like:
-    # - Country Name
-    # - Country Code
-    # - [Stuff I don't care about]
-    # - GDP for 1960
-    # - GDP for 1961
-    # - GDP for 1962
-    # - ...
-    # - GDP for 2022
-    #
-    # ...but I want this instead:
-    # - Country Name
-    # - Country Code
-    # - Year
-    # - GDP
-    #
-    # So let's pivot all those year-columns into two: Year and GDP
-    gdp_df = raw_gdp_df.melt(
-        ['Country Code'],
-        [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
-        'Year',
-        'GDP',
-    )
-
-    # Convert years from string to integers
-    gdp_df['Year'] = pd.to_numeric(gdp_df['Year'])
-
-    return gdp_df
-
-gdp_df = get_gdp_data()
 
 def get_lte_countries():
     """Grab LTE Download data from a CSV file.
@@ -77,7 +33,27 @@ def get_lte_countries():
     return raw_lte_countries_df
 
 
+
+
+def get_lte_sectors():
+    """Grab LTE Download data from a CSV file.
+
+    This uses caching to avoid having to read the file every time. If we were
+    reading from an HTTP endpoint instead of a file, it's a good idea to set
+    a maximum age to the cache with the TTL argument: @st.cache_data(ttl='1d')
+    """
+
+    # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
+    DATA_FILENAME = Path(__file__).parent/'data/lte_sectors.csv'
+    raw_lte_sectors_df = pd.read_csv(DATA_FILENAME)
+
+    # Convert years from string to integers
+    raw_lte_sectors_df['year'] = pd.to_numeric(raw_lte_sectors_df['year'])
+
+    return raw_lte_sectors_df
+
 lte_countries_df = get_lte_countries()
+lte_sectors_df = get_lte_sectors()
 # -----------------------------------------------------------------------------
 # Draw the actual page
 
@@ -123,6 +99,11 @@ filtered_lte_df = lte_countries_df[
     & (from_year <= lte_countries_df['year'])
 ]
 
+# Filter the data
+filtered_lte_sector_df = lte_sectors_df[
+    (lte_countries_df['year'] <= to_year)
+    & (from_year <= lte_countries_df['year'])
+]
 st.header('LTE Download Data over time', divider='gray')
 
 ''
@@ -143,6 +124,18 @@ st.bar_chart(
     x='year',
     y='dls',
     color='country',
+    use_container_width=True
+)
+
+'''
+## By sector
+'''
+
+st.bar_chart(
+    filtered_lte_sector_df,
+    x='year',
+    y='dls',
+    color='sector',
     use_container_width=True
 )
 
